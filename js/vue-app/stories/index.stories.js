b8652c048387565d914b6fe8cfa3cc646e433bab
Brian Horakh <brianh@elastic.ventures>
2019-07-03 01:33:10 +1000

    storybook installed in default vue-cli app

---
+++ js/vue-app/stories/index.stories.js
@@ -0,0 +1,43 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+import { storiesOf } from '@storybook/vue';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import MyButton from './MyButton';
+import Welcome from './Welcome';
+
+storiesOf('Welcome EV', module).add('to Storybook', () => ({
+  components: { Welcome },
+  template: '<welcome :showApp="action" />',
+  methods: { action: linkTo('Button') },
+}));
+
+storiesOf('Button', module)
+  .add('with text', () => ({
+    components: { MyButton },
+    template: '<my-button @click="action">Hello Button</my-button>',
+    methods: { action: action('clicked') },
+  }))
+  .add('with JSX', () => ({
+    components: { MyButton },
+    render(h) {
+      return <my-button onClick={this.action}>With JSX</my-button>;
+    },
+    methods: { action: linkTo('clicked') },
+  }))
+  .add('with some emoji', () => ({
+    components: { MyButton },
+    template: '<my-button @click="action">😀 😎 👍 💯</my-button>',
+    methods: { action: action('clicked') },
+  }));
+
+
+
+storiesOf('Button2', module)
+  .add('with text', () => '<my-button>with text</my-button>')
+  .add('with emoji', () => '<my-button>😀 😎 👍 💯</my-button>')
+  .add('as a component', () => ({
+    components: { MyButton },
+    template: '<my-button :rounded="true">rounded</my-button>'
+  }));
