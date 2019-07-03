600a51edd3cc995748c1da4cfc859a9461c10fc8
Brian Horakh <brianh@elastic.ventures>
2019-07-03 10:18:44 +1000

    vue-svgicon + tracking src/ dir

---
+++ js/vue-app/src/views/SVG.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>This is SVG page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import VueSvgIcon, { PluginOptions } from 'vue-svgicon'
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class Home extends Vue {}
+</script>
